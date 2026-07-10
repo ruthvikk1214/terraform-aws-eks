ece15e5e3037017453cb772ba70453e0627d4baf
ruthvikk1214 <ruthvikk1214@gmail.com>
2026-07-10 15:50:27 +0530

    added sg rules

--- 20-sg-rules/data.tf
+++ 20-sg-rules/data.tf
@@ -49,6 +49,6 @@ data "aws_ssm_parameter" "openvpn_sg_id" {
 data "aws_ssm_parameter" "ingress_alb_sg_id" {
   name = "/${var.project}/${var.environment}/ingress_alb_sg_id"
 }
-data "aws_ssm_paramerer" "ingress_sg_id" {
+data "aws_ssm_parameter" "ingress_sg_id" {
   name = "/${var.project}/${var.environment}/ingress_sg_id"
 }
