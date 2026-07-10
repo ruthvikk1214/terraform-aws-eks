e10426130d051f2e20861841f5d8d38c0dac019a
ruthvikk1214 <ruthvikk1214@gmail.com>
2026-07-10 17:21:32 +0530

    added key

--- 30-jenkins/locals.tf
+++ 30-jenkins/locals.tf
@@ -1,6 +1,7 @@
 locals {
   ami_id =  data.aws_ami.joindevops.id
   private_subnet_id = split(",", data.aws_ssm_parameter.private_subnet_ids.value)[0]
+  public_subnet_id = split(",", data.aws_ssm_parameter.public_subnet_ids.value)[0]
   jenkins_sg_id             = data.aws_ssm_parameter.jenkins_sg_id.value
-  jenkins_agent_sg_id       = data.aws_ssm_parameter.jenkins_agent_sg_id.value
+  jenkins_agent_sg_id       = data.aws_ssm_parameter.jenkins_agent_sg_id.value  
 }
