ba6e907036fd299e3e8d658f4904dfe14e657d21
ruthvikk1214 <ruthvikk1214@gmail.com>
2026-07-10 17:22:25 +0530

    added key

--- 30-jenkins/data.tf
+++ 30-jenkins/data.tf
@@ -26,3 +26,6 @@ data "aws_ami" "joindevops" {
 data "aws_ssm_parameter" "private_subnet_ids" {
   name = "/roboshop/dev/private_subnet_ids"
 }
+data "aws_ssm_parameter" "public_subnet_ids" {
+  name = "/roboshop/dev/public_subnet_ids"
+}
