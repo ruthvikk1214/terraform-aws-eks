fe3cbeaf5bb8465b6318fe61b1d4f1bdcbf3ab95
ruthvikk1214 <ruthvikk1214@gmail.com>
2026-07-10 16:21:31 +0530

    added key

--- 30-jenkins/provider.tf
+++ 30-jenkins/provider.tf
@@ -7,7 +7,7 @@ terraform {
   }
   backend "s3" {
     bucket = "remote-state-roboshop-dev-rk1214"
-    key    = "terraform-aws-eks-vpc"
+    key    = "terraform-aws-eks/jenkins"
     region = "us-east-1"
     ##dynamodb_table = "terraform-lock-table" # Optional: for locking
     encrypt      = true # Recommended
