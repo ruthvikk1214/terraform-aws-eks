e353f8b12cc34411e4da43bee2d8f9cbe2dd5f1b
ruthvikk1214 <ruthvikk1214@gmail.com>
2026-07-10 15:34:08 +0530

    added sg rules

--- 20-sg-rules/main.tf
+++ 20-sg-rules/main.tf
@@ -178,12 +178,3 @@ resource "aws_security_group_rule" "eks_control_plane_bastion" {
   source_security_group_id = local.bastion_sg_id
   security_group_id = local.eks_control_plane_sg_id
 }
-resource "aws_security_group_rule" "eks_control_plane_bastion" {
-  type              = "ingress"
-  from_port         = 443
-  to_port           = 443
-  protocol          = "tcp"
-  # Where traffic is coming from
-  source_security_group_id = local.bastion_sg_id
-  security_group_id = local.eks_control_plane_sg_id
-}
