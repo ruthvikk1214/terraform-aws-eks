bb2c26a886765259c5e9b0fe8cfdf61df9c4ce4d
ruthvikk1214 <ruthvikk1214@gmail.com>
2026-07-10 23:47:19 +0530

    added jenkins

--- 30-jenkins/variables.tf
+++ 30-jenkins/variables.tf
@@ -8,3 +8,14 @@ variable "environment" {
   type        = string
   default     = "dev"
 }
+variable "domain_name" {
+  description = "The domain name for the Route 53 record."
+  type        = string
+  default     = "rk1214.in"
+}
+
+variable "zone_id" {
+  description = "The Route 53 hosted zone ID."
+  type        = string
+  default     = "Z031906510N5GWM6MW07L"
+}
