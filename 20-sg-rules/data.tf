bf6d0ea3afb05ed17d1e6ec1ffd4a341041bfbca
ruthvikk1214 <ruthvikk1214@gmail.com>
2026-07-10 15:48:17 +0530

    added sg rules

--- 20-sg-rules/data.tf
+++ 20-sg-rules/data.tf
@@ -43,3 +43,9 @@ data "aws_ssm_parameter" "jenkins_agent_sg_id" {
 data "aws_ssm_parameter" "runner_sg_id" {
   name = "/${var.project}/${var.environment}/runner_sg_id"
 }
+data "aws_ssm_parameter" "openvpn_sg_id" {
+  name = "/${var.project}/${var.environment}/openvpn_sg_id"
+}
+data "aws_ssm_parameter" "ingress_alb_sg_id" {
+  name = "/${var.project}/${var.environment}/ingress_alb_sg_id"
+}
--- 20-sg-rules/main.tf
+++ 20-sg-rules/main.tf
@@ -74,28 +74,28 @@ resource "aws_security_group_rule" "openvpn_public_943" { #openvpn accepting con
 }
 
 resource "aws_security_group_rule" "eks_node_bastion" { #openvpn accepting connections from internet to serve requests
-  type              = "ingress"
-  from_port         = 22
-  to_port           = 22
-  protocol          = "tcp"
+  type                     = "ingress"
+  from_port                = 22
+  to_port                  = 22
+  protocol                 = "tcp"
   source_security_group_id = local.bastion_sg_id
-  security_group_id = local.eks_node_sg_id
+  security_group_id        = local.eks_node_sg_id
 }
 resource "aws_security_group_rule" "eks_control_plane_eks_node" { #openvpn accepting connections from internet to serve requests
-  type              = "ingress"
-  from_port         = 0
-  to_port           = 0
-  protocol          = "-1"
+  type                     = "ingress"
+  from_port                = 0
+  to_port                  = 0
+  protocol                 = "-1"
   source_security_group_id = local.eks_node_sg_id
-  security_group_id = local.eks_control_plane_sg_id
+  security_group_id        = local.eks_control_plane_sg_id
 }
 resource "aws_security_group_rule" "eks_node_eks_control_plane" { #openvpn accepting connections from internet to serve requests
-  type              = "ingress"
-  from_port         = 0
-  to_port           = 0
-  protocol          = "-1"
+  type                     = "ingress"
+  from_port                = 0
+  to_port                  = 0
+  protocol                 = "-1"
   source_security_group_id = local.eks_control_plane_sg_id
-  security_group_id = local.eks_node_sg_id
+  security_group_id        = local.eks_node_sg_id
 }
 resource "aws_security_group_rule" "eks_node_vpc_cidr" { #openvpn accepting connections from internet to serve requests
   type              = "ingress"
@@ -107,74 +107,74 @@ resource "aws_security_group_rule" "eks_node_vpc_cidr" { #openvpn accepting conn
 }
 
 resource "aws_security_group_rule" "jenkins_agent_ssh" {
-  type              = "ingress"
-  from_port         = 22
-  to_port           = 22
-  protocol          = "tcp" # all traffic
+  type      = "ingress"
+  from_port = 22
+  to_port   = 22
+  protocol  = "tcp" # all traffic
   # VPC CIDR
-  cidr_blocks = ["0.0.0.0/0"]
+  cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = local.jenkins_agent_sg_id
 }
 resource "aws_security_group_rule" "jenkins_ssh" {
-  type              = "ingress"
-  from_port         = 22
-  to_port           = 22
-  protocol          = "tcp" # all traffic
+  type      = "ingress"
+  from_port = 22
+  to_port   = 22
+  protocol  = "tcp" # all traffic
   # VPC CIDR
-  cidr_blocks = ["0.0.0.0/0"]
+  cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = local.jenkins_sg_id
 }
 resource "aws_security_group_rule" "jenkins_public" {
-  type              = "ingress"
-  from_port         = 8080
-  to_port           = 8080
-  protocol          = "tcp" # all traffic
+  type      = "ingress"
+  from_port = 8080
+  to_port   = 8080
+  protocol  = "tcp" # all traffic
   # VPC CIDR
-  cidr_blocks = ["0.0.0.0/0"]
+  cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = local.jenkins_sg_id
 }
 resource "aws_security_group_rule" "eks_control_plane_runner" {
-  type              = "ingress"
-  from_port         = 443
-  to_port           = 443
-  protocol          = "tcp"
+  type      = "ingress"
+  from_port = 443
+  to_port   = 443
+  protocol  = "tcp"
   # Where traffic is coming from
   source_security_group_id = local.runner_sg_id
-  security_group_id = local.eks_control_plane_sg_id
+  security_group_id        = local.eks_control_plane_sg_id
 }
 resource "aws_security_group_rule" "runner_ssh" {
-  type              = "ingress"
-  from_port         = 22
-  to_port           = 22
-  protocol          = "tcp"
+  type      = "ingress"
+  from_port = 22
+  to_port   = 22
+  protocol  = "tcp"
   # Where traffic is coming from
-  cidr_blocks = ["0.0.0.0/0"]
+  cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = local.runner_sg_id
 }
 resource "aws_security_group_rule" "eks_control_plane_jenkins_agent" {
-  type              = "ingress"
-  from_port         = 443
-  to_port           = 443
-  protocol          = "tcp"
+  type      = "ingress"
+  from_port = 443
+  to_port   = 443
+  protocol  = "tcp"
   # Where traffic is coming from
   source_security_group_id = local.jenkins_agent_sg_id
-  security_group_id = local.eks_control_plane_sg_id
+  security_group_id        = local.eks_control_plane_sg_id
 }
 resource "aws_security_group_rule" "mysql_eks_node" {
-  type              = "ingress"
-  from_port         = 3306
-  to_port           = 3306
-  protocol          = "tcp"
+  type      = "ingress"
+  from_port = 3306
+  to_port   = 3306
+  protocol  = "tcp"
   # Where traffic is coming from
   source_security_group_id = local.eks_node_sg_id
-  security_group_id = local.mysql_sg_id
+  security_group_id        = local.mysql_sg_id
 }
 resource "aws_security_group_rule" "eks_control_plane_bastion" {
-  type              = "ingress"
-  from_port         = 443
-  to_port           = 443
-  protocol          = "tcp"
+  type      = "ingress"
+  from_port = 443
+  to_port   = 443
+  protocol  = "tcp"
   # Where traffic is coming from
   source_security_group_id = local.bastion_sg_id
-  security_group_id = local.eks_control_plane_sg_id
+  security_group_id        = local.eks_control_plane_sg_id
 }
