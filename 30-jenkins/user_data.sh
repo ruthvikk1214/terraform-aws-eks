1351b3f94953269c9c83c4929f70af143a5b103e
ruthvikk1214 <ruthvikk1214@gmail.com>
2026-07-11 16:29:05 +0530

    modified bootstrap

--- 30-jenkins/user_data.sh
+++ 30-jenkins/user_data.sh
@@ -31,11 +31,13 @@ systemctl start jenkins
 # -------------------------------
 # Configure Docker
 # -------------------------------
-systemctl enable docker
-systemctl start docker
-
-usermod -aG docker ec2-user
-usermod -aG docker jenkins
+sudo dnf -y install dnf-plugins-core
+sudo dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
+sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo usermod -aG docker ec2-user
+sudo usermod -aG docker jenkins
 
 # -------------------------------
 # Install eksctl
