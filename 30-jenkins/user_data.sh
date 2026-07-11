6c056039b37f51deede108dbc36506f14fba60c0
ruthvikk1214 <ruthvikk1214@gmail.com>
2026-07-12 00:02:55 +0530

    added volume

--- 30-jenkins/user_data.sh
+++ 30-jenkins/user_data.sh
@@ -1,12 +1,34 @@
 #!/bin/bash
 set -e
 
+
 # -------------------------------
-# Extend /var filesystem
+# Install eksctl
 # -------------------------------
-# growpart /dev/nvme0n1 4
-# pvresize /dev/nvme0n1p4
-# lvextend -r -l +100%FREE /dev/RootVG/varVol
+ARCH=amd64
+PLATFORM=$(uname -s)_${ARCH}
+
+curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_${PLATFORM}.tar.gz"
+
+tar -xzf eksctl_${PLATFORM}.tar.gz -C /tmp
+
+install -m 0755 /tmp/eksctl /usr/local/bin/eksctl
+
+rm -f eksctl_${PLATFORM}.tar.gz
+rm -f /tmp/eksctl
+
+# -------------------------------
+# Install kubectl
+# -------------------------------
+curl -LO "https://s3.us-west-2.amazonaws.com/amazon-eks/1.35.2/2026-02-27/bin/linux/amd64/kubectl"
+
+chmod +x kubectl
+
+mv kubectl /usr/local/bin/
+# -------------------------------
+pvcreate /dev/nvme1n1
+vgextend RootVG /dev/nvme1n1
+lvextend -r -l +100%FREE /dev/RootVG/varVol
 
 # -------------------------------
 # Install required packages
@@ -39,29 +61,7 @@ sudo systemctl enable docker
 sudo usermod -aG docker ec2-user
 sudo usermod -aG docker jenkins
 
-# -------------------------------
-# Install eksctl
-# -------------------------------
-ARCH=amd64
-PLATFORM=$(uname -s)_${ARCH}
 
-curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_${PLATFORM}.tar.gz"
-
-tar -xzf eksctl_${PLATFORM}.tar.gz -C /tmp
-
-install -m 0755 /tmp/eksctl /usr/local/bin/eksctl
-
-rm -f eksctl_${PLATFORM}.tar.gz
-rm -f /tmp/eksctl
-
-# -------------------------------
-# Install kubectl
-# -------------------------------
-curl -LO "https://s3.us-west-2.amazonaws.com/amazon-eks/1.35.2/2026-02-27/bin/linux/amd64/kubectl"
-
-chmod +x kubectl
-
-mv kubectl /usr/local/bin/
 
 # -------------------------------
 # Verify installations
