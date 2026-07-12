e179f1e0f2af1abcf1e2cd353771a48007c2f406
ruthvikk1214 <ruthvikk1214@gmail.com>
2026-07-12 18:54:57 +0530

    chore(jenkins): enable user_data_replace_on_change for jenkins instance

--- 30-jenkins/main.tf
+++ 30-jenkins/main.tf
@@ -3,7 +3,8 @@ resource "aws_instance" "jenkins" {
   instance_type          = "t2.micro"
   subnet_id              = local.public_subnet_id
   vpc_security_group_ids = [local.jenkins_sg_id]
-  user_data              = file("${path.module}/user_data.sh")
+   user_data              = file("${path.module}/user_data.sh")
+   user_data_replace_on_change = true
   root_block_device {
     volume_size = 30
     volume_type = "gp3"
