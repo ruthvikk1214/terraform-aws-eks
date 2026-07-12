2114174ac8fe196e9effb0dd5b4a99a9e8357320
ruthvikk1214 <ruthvikk1214@gmail.com>
2026-07-12 14:08:37 +0530

    added jenkins agent and sonarqube

--- 30-jenkins/main.tf
+++ 30-jenkins/main.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "jenkins" {
   subnet_id              = local.public_subnet_id
   vpc_security_group_ids = [local.jenkins_sg_id]
   user_data              = file("${path.module}/user_data.sh")
-   root_block_device {
+  root_block_device {
     volume_size = 30
     volume_type = "gp3"
     encrypted   = true
@@ -25,53 +25,53 @@ resource "aws_ebs_volume" "jenkins" {
   }
 }
 
-resource "aws_volume_attachment" "jenkins" {
-  device_name = "/dev/sdf"
-  volume_id   = aws_ebs_volume.jenkins.id
-  instance_id = aws_instance.jenkins.id
-}
-resource "aws_instance" "sonarqube" {
-  count = var.sonar ? 1 : 0
-  ami           = local.sonar_ami_id
-  instance_type = "t3.large"
-  vpc_security_group_ids = [local.sonar_sg_id]
-  subnet_id = local.public_subnet_id #replace your Subnet in default VPC
-  key_name = "daws-88s"
-  # need more for terraform
-  root_block_device {
-    volume_size = 20
-    volume_type = "gp3" # or "gp2", depending on your preference
-  }
-  tags = merge(
-    local.common_tags,
-    {
-        Name = "${var.project}-${var.environment}-sonar"
-    }
-  )
-}
-resource "aws_instance" "jenkins_agent" {
-  count = var.jenkins ? 1 : 0
-  ami           = local.ami_id
-  instance_type = "t3.micro"
-  subnet_id = local.public_subnet_id
-  vpc_security_group_ids = [ local.jenkins_agent_sg_id ]
-  user_data = file("jenkins-agent.sh")
+# resource "aws_volume_attachment" "jenkins" {
+#   device_name = "/dev/sdf"
+#   volume_id   = aws_ebs_volume.jenkins.id
+#   instance_id = aws_instance.jenkins.id
+# }
+# resource "aws_instance" "sonarqube" {
+#   count = var.sonar ? 1 : 0
+#   ami           = local.sonar_ami_id
+#   instance_type = "t3.large"
+#   vpc_security_group_ids = [local.sonar_sg_id]
+#   subnet_id = local.public_subnet_id #replace your Subnet in default VPC
+#   key_name = "daws-88s"
+#   # need more for terraform
+#   root_block_device {
+#     volume_size = 20
+#     volume_type = "gp3" # or "gp2", depending on your preference
+#   }
+#   tags = merge(
+#     local.common_tags,
+#     {
+#         Name = "${var.project}-${var.environment}-sonar"
+#     }
+#   )
+# }
+# resource "aws_instance" "jenkins_agent" {
+#   count = var.jenkins ? 1 : 0
+#   ami           = local.ami_id
+#   instance_type = "t3.micro"
+#   subnet_id = local.public_subnet_id
+#   vpc_security_group_ids = [ local.jenkins_agent_sg_id ]
+#   user_data = file("jenkins-agent.sh")
 
-  root_block_device {
-    volume_size = 50
-    volume_type = "gp3"
-    tags = merge(
-      {
-          Name = "${var.project}-${var.environment}-jenkins-agent"
-      },
-    local.common_tags
-    )
-  }
+#   root_block_device {
+#     volume_size = 50
+#     volume_type = "gp3"
+#     tags = merge(
+#       {
+#           Name = "${var.project}-${var.environment}-jenkins-agent"
+#       },
+#     local.common_tags
+#     )
+#   }
 
-  tags = merge(
-    {
-        Name = "${var.project}-${var.environment}-jenkins-agent"
-    },
-    local.common_tags
-  )
-}
+#   tags = merge(
+#     {
+#         Name = "${var.project}-${var.environment}-jenkins-agent"
+#     },
+#     local.common_tags
+#   )
+# }
