d195abb4e5dacc8120eb3aba895b41d0b80a09f1
ruthvikk1214 <ruthvikk1214@gmail.com>
2026-07-12 13:43:20 +0530

    added jenkins agent and sonarqube

--- 30-jenkins/data.tf
+++ 30-jenkins/data.tf
@@ -5,8 +5,8 @@ data "aws_ssm_parameter" "jenkins_agent_sg_id" {
   name = "/roboshop/dev/jenkins_agent_sg_id"
 }
 data "aws_ami" "joindevops" {
-  most_recent      = true
-  owners           = ["973714476881"]
+  most_recent = true
+  owners      = ["973714476881"]
 
   filter {
     name   = "name"
@@ -47,7 +47,4 @@ data "aws_ami" "sonarqube" {
     name   = "architecture"
     values = ["x86_64"]
   }
-}
-data "aws_ssm_parameter" "jenkins_agent_sg_id" {
-    name = "/${var.project}/${var.environment}/jenkins_agent_sg_id"
 }
