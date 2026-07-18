93af2288ae8ffd63e0612c9964eb3484d32e8a0b
ruthvikk1214 <ruthvikk1214@gmail.com>
2026-07-19 01:07:24 +0530

    modified bootstrap

--- 30-jenkins/jenkins-agent.sh
+++ 30-jenkins/jenkins-agent.sh
@@ -16,14 +16,6 @@ yum install fontconfig java-21-openjdk -y
 dnf module enable nodejs:20 -y
 dnf install nodejs -y
 
-# Docker
-dnf -y install dnf-plugins-core
-dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
-dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
-systemctl start docker
-systemctl enable docker
-usermod -aG docker ec2-user
-
 # kubectl
 curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.35.2/2026-02-27/bin/linux/amd64/kubectl
 chmod +x ./kubectl
